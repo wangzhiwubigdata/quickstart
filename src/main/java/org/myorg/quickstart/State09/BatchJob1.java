b75a9e8649d6285ff1a983ddf23b18023db790b2
wangzhiwubigdata <2827873682@qq.com>
2020-05-06 23:06:52 +0800

    1

--- src/main/java/org/myorg/quickstart/State09/BatchJob1.java
+++ src/main/java/org/myorg/quickstart/State09/BatchJob1.java
@@ -19,14 +19,20 @@
 package org.myorg.quickstart.State09;
 
 import org.apache.flink.api.common.functions.RichFlatMapFunction;
+import org.apache.flink.api.common.state.StateTtlConfig;
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.time.Time;
 import org.apache.flink.api.common.typeinfo.TypeHint;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.runtime.state.filesystem.FsStateBackend;
+import org.apache.flink.runtime.state.memory.MemoryStateBackend;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
+
+import static org.apache.flink.runtime.state.memory.MemoryStateBackend.DEFAULT_MAX_STATE_SIZE;
 
 public class BatchJob1 {
 
@@ -34,6 +40,8 @@ public class BatchJob1 {
 	public static void main(String[] args) throws Exception {
 
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        env.setStateBackend(new FsStateBackend("hdfs://namenode:40010/flink/checkpoints", false));
 
 		env.fromElements(Tuple2.of(1L, 3L), Tuple2.of(1L, 5L), Tuple2.of(1L, 7L), Tuple2.of(1L, 5L), Tuple2.of(1L, 2L))
 				.keyBy(0)
@@ -50,8 +58,13 @@ public class BatchJob1 {
         private transient ValueState<Tuple2<Long, Long>> sum;
         public void flatMap(Tuple2<Long, Long> input, Collector<Tuple2<Long, Long>> out) throws Exception {
 
+            Tuple2<Long, Long> currentSum;
             // 访问ValueState
-            Tuple2<Long, Long> currentSum = sum.value();
+            if(sum.value()==null){
+                currentSum = Tuple2.of(0L, 0L);
+            }else {
+                currentSum = sum.value();
+            }
 
             // 更新
             currentSum.f0 += 1;
@@ -74,9 +87,19 @@ public class BatchJob1 {
         ValueStateDescriptor<Tuple2<Long, Long>> descriptor =
                 new ValueStateDescriptor<>(
                         "average", // state的名字
-                        TypeInformation.of(new TypeHint<Tuple2<Long, Long>>() {}),
-                        Tuple2.of(0L, 0L)); // 设置默认值
+                        TypeInformation.of(new TypeHint<Tuple2<Long, Long>>() {})
+                        ); // 设置默认值
+
+
+        StateTtlConfig ttlConfig = StateTtlConfig
+                .newBuilder(Time.seconds(10))
+                .setUpdateType(StateTtlConfig.UpdateType.OnCreateAndWrite)
+                .setStateVisibility(StateTtlConfig.StateVisibility.NeverReturnExpired)
+                .build();
+
+        descriptor.enableTimeToLive(ttlConfig);
+
         sum = getRuntimeContext().getState(descriptor);
-    }
-}
+        }
+	}
 }
