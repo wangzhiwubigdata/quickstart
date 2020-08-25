e4525a2cbad90c5682218456d4f16b9346251a2f
wangzhiwubigdata <2827873682@qq.com>
2020-08-25 21:10:50 +0800

    新增工具类

---
+++ src/main/java/org/myorg/quickstart/shizhan02/DateUtil.java
@@ -0,0 +1,24 @@
+package org.myorg.quickstart.shizhan02;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateUtil {
+
+
+    public static String timeStampToDate(Long timestamp){
+
+        ThreadLocal<SimpleDateFormat> threadLocal
+                = ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+        String format = threadLocal.get().format(new Date(timestamp));
+        return format.substring(0,10);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(timeStampToDate(System.currentTimeMillis()));
+    }
+
+}
